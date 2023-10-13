341b3179b6c97a4d85b6787d150de9857eff55fc
joebakid <josephbawo@gmail.com>
2023-10-14 00:52:15 +0100

    bg-color

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const light = document.getElementById("nav-bar__link-light");
 const body = document.querySelector("body");
+const bar = document.querySelectorAll(".bar-tech");
 
 light.addEventListener("click", toggle);
 dark.addEventListener("click", toggle);
@@ -10,9 +11,15 @@ function toggle() {
     body.style.background = "black";
     body.style.color = "white";
     body.style.transition = "all 0.5s ease-in";
+    bar.forEach(function (bar) {
+      bar.style.background = "white";
+    });
   } else {
     light.textContent = "Light";
     body.style.background = "white";
     body.style.color = "black";
+    bar.forEach(function (bar) {
+      bar.style.background = "black";
+    });
   }
 }
