046a3509421530f338f104adb4224810722b7d91
joebakid <josephbawo@gmail.com>
2023-10-14 00:41:14 +0100

    added light and dark-mode

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
- 
+const light = document.getElementById("nav-bar__link-light");
+const body = document.querySelector("body");
+
+light.addEventListener("click", toggle);
+dark.addEventListener("click", toggle);
+
+function toggle() {
+  if (light.textContent === "Light") {
+    light.textContent = "Dark";
+    body.style.background = "black";
+    body.style.color = "white";
+    body.style.transition = "all 0.5s ease-in";
+  } else {
+    light.textContent = "Light";
+    body.style.background = "white";
+    body.style.color = "black";
+  }
+}
