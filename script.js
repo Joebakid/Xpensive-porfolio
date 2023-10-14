2a5ab817fae6e16414cd5981e0344d1aa3ec0761
joebakid <josephbawo@gmail.com>
2023-10-14 07:57:33 +0100

    added animation

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const light = document.getElementById("nav-bar__link-light");
 const body = document.querySelector("body");
 const bar = document.querySelectorAll(".bar-tech");
 const mode = localStorage.getItem("mode"); //STORE USER PREFERNCE IN BROWSER
+const subText = document.querySelector(".main-content__sub-text");
 
 //LIGHT-MODE FUNC
 function enableLightMode() {
   body.style.background = "white";
   body.style.color = "black";
+  subText.style.color = "black";
   bar.forEach(function (bar) {
     bar.style.background = "black";
   });
@@ -18,6 +20,7 @@ function enableDarkMode() {
   body.style.background = "black";
   body.style.color = "white";
   body.style.transition = "all 0.5s ease-in";
+  subText.style.color = "white";
   bar.forEach(function (bar) {
     bar.style.background = "white";
   });
