0e4f24174463de8789711496d3af29f5ef46dc8f
joebakid <josephbawo@gmail.com>
2023-10-14 01:12:49 +0100

    local storage to store mode

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const light = document.getElementById("nav-bar__link-light");
 const body = document.querySelector("body");
 const bar = document.querySelectorAll(".bar-tech");
+const mode = localStorage.getItem("mode"); //STORE USER PREFERNCE IN BROWSER
+
+//LOCAL STORAGE LOGIC
+if (mode === "dark") {
+  enableDarkMode();
+} else {
+  enableLightMode();
+}
+
+//LIGHT-MODE FUNC
+function enableLightMode() {
+  body.style.background = "white";
+  body.style.color = "black";
+  bar.forEach(function (bar) {
+    bar.style.background = "black";
+  });
+  localStorage.setItem("mode", "light");
+}
+
+//DARK-MODE FUNC
+function enableDarkMode() {
+  body.style.background = "black";
+  body.style.color = "white";
+  body.style.transition = "all 0.5s ease-in";
+  bar.forEach(function (bar) {
+    bar.style.background = "white";
+  });
+  localStorage.setItem("mode", "dark");
+}
 
 light.addEventListener("click", toggle);
-dark.addEventListener("click", toggle);
 
 function toggle() {
   if (light.textContent === "Light") {
     light.textContent = "Dark";
-    body.style.background = "black";
-    body.style.color = "white";
-    body.style.transition = "all 0.5s ease-in";
-    bar.forEach(function (bar) {
-      bar.style.background = "white";
-    });
+    enableDarkMode();
   } else {
     light.textContent = "Light";
-    body.style.background = "white";
-    body.style.color = "black";
-    bar.forEach(function (bar) {
-      bar.style.background = "black";
-    });
+    enableLightMode();
   }
 }
